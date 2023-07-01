635132173d0412324825162eb489b257fbb25781
Ramonprog <programador.ramon@gmail.com>
2023-07-01 19:06:28 -0300

    header com avatar e input + estrutura do chatList

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {useState, useEffect} from 'react'
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import ChatListItem from "./components/ChatListItem/chatListItem";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [chatList, setChatList] = useState([{},{},{},{}])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-window">
+        <div className="sidebar">
+          <header>
+            <img
+              className="header-avatar"
+              src="https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg"
+              alt="avatar"
+            />
+            <div className="header-buttons">
+              <div className="header--btn">
+                <DonutLargeIcon style={{ color: "#919191" }} />
+                <ChatIcon style={{ color: "#919191", marginLeft:'8px' }} />
+                <MoreVertIcon style={{ color: "#919191",marginRight:'8px' }} />
+              </div>
+            </div>
+          </header>
+          <div className="search">
+            <div className="search--input">
+              <SearchIcon style={{ color: "#919191" }} fontSize="small" />
+              <input
+                type="search"
+                placeholder="Procurar ou começar uma nova conversa"
+              />
+            </div>
+          </div>
+          <div className="chatlist">
+            {chatList.map((item, key) => (
+              <ChatListItem key={key}/>
+            ))}
+          </div>
+        </div>
+        <div className="contentarea">...</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ChatListItem/ChatListItem.tsx
@@ -0,0 +1,9 @@
+import './chatListItem.css'
+
+const ChatListItem = () => {
+  return (
+    <div>chatListItem</div>
+  )
+}
+
+export default ChatListItem
