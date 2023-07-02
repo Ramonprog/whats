0e62fe374ac5f94f48b1de38db926247abb4c710
Ramonprog <programador.ramon@gmail.com>
2023-07-02 20:59:07 -0300

    emoji picker

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -2,12 +2,19 @@ import "./Chatwindow.css";
 import SearchIcon from "@mui/icons-material/Search";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-import CloseIcon from '@mui/icons-material/Close';
-import SendIcon from '@mui/icons-material/Send';
-import MicNoneIcon from '@mui/icons-material/MicNone';
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
+import CloseIcon from "@mui/icons-material/Close";
+import SendIcon from "@mui/icons-material/Send";
+import MicNoneIcon from "@mui/icons-material/MicNone";
+
+import EmojiPicker from "emoji-picker-react";
+import { useState } from "react";
 
 const ChatWindow = () => {
+  const [emojiOpen, setEmojiOpen] = useState(false);
+
+  const handleEmojiClick = () => {};
+
   return (
     <div className="chatWindow">
       <div className="window--header">
@@ -33,14 +40,33 @@ const ChatWindow = () => {
         </div>
       </div>
       <div className="window--body">.</div>
+      <div
+        className="window--emojiarea"
+      >
+        {emojiOpen && (
+          <div className="emoji-picker-container" >
+            <EmojiPicker 
+            searchDisabled 
+            onEmojiClick={handleEmojiClick}
+            height={'300px'}
+            skinTonesDisabled
+            
+            />
+          </div>
+        )}
+      </div>
       <div className="window--footer">
         <div className="window--left">
-          <div className="window--btn">
-            <SentimentSatisfiedAltIcon style={{ color: "#919191" }} />
+          <div className="window--btn" onClick={() => setEmojiOpen(!emojiOpen)}>
+            <SentimentSatisfiedAltIcon style={{ color:emojiOpen? '#009688' : "#919191" }} />
           </div>
         </div>
         <div className="window--center">
-          <input type="text"  className="window-input" placeholder="Digite uma mensagem"/>
+          <input
+            type="text"
+            className="window-input"
+            placeholder="Digite uma mensagem"
+          />
         </div>
         <div className="window--right">
           <div className="window--btn">
