f2e9995c8f9f3c6f7fd589390fb342a642269e52
Ramonprog <programador.ramon@gmail.com>
2023-07-03 22:14:38 -0300

    pegando emoju e mudando a cor do mic

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -3,17 +3,45 @@ import SearchIcon from "@mui/icons-material/Search";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
-import CloseIcon from "@mui/icons-material/Close";
 import SendIcon from "@mui/icons-material/Send";
 import MicNoneIcon from "@mui/icons-material/MicNone";
-
 import EmojiPicker from "emoji-picker-react";
 import { useState } from "react";
 
 const ChatWindow = () => {
   const [emojiOpen, setEmojiOpen] = useState(false);
+  const [text, setText] = useState("");
+  const [listening, setListening] = useState(false)
 
-  const handleEmojiClick = () => {};
+  let recognition: SpeechRecognition | undefined;
+  let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  
+  if (SpeechRecognition !== undefined) {
+    recognition = new SpeechRecognition();
+  }
+
+  const handleEmojiClick = (emoji: any) => {
+    console.log(emoji);
+    setText((prev) => prev + emoji.emoji);
+  };
+
+  const handleSend = () => {};
+
+
+  const handleMic = () => {
+    if (recognition !== undefined) {
+      recognition.onstart = () => {
+        setListening(true);
+      };
+      recognition.onend = () => {
+        setListening(false);
+      };
+      recognition.onresult = (e) => {
+        setText(e.results[0][0].transcript);
+      };
+      recognition.start();
+    }
+  };
 
   return (
     <div className="chatWindow">
@@ -40,17 +68,14 @@ const ChatWindow = () => {
         </div>
       </div>
       <div className="window--body">.</div>
-      <div
-        className="window--emojiarea"
-      >
+      <div className="window--emojiarea">
         {emojiOpen && (
-          <div className="emoji-picker-container" >
-            <EmojiPicker 
-            searchDisabled 
-            onEmojiClick={handleEmojiClick}
-            height={'300px'}
-            skinTonesDisabled
-            
+          <div className="emoji-picker-container">
+            <EmojiPicker
+              searchDisabled
+              onEmojiClick={handleEmojiClick}
+              height={"300px"}
+              skinTonesDisabled
             />
           </div>
         )}
@@ -58,7 +83,9 @@ const ChatWindow = () => {
       <div className="window--footer">
         <div className="window--left">
           <div className="window--btn" onClick={() => setEmojiOpen(!emojiOpen)}>
-            <SentimentSatisfiedAltIcon style={{ color:emojiOpen? '#009688' : "#919191" }} />
+            <SentimentSatisfiedAltIcon
+              style={{ color: emojiOpen ? "#009688" : "#919191" }}
+            />
           </div>
         </div>
         <div className="window--center">
@@ -66,11 +93,17 @@ const ChatWindow = () => {
             type="text"
             className="window-input"
             placeholder="Digite uma mensagem"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
           />
         </div>
         <div className="window--right">
           <div className="window--btn">
-            <SendIcon style={{ color: "#919191" }} />
+            {text ? (
+              <SendIcon style={{ color: "#919191" }} />
+            ) : (
+              <MicNoneIcon onClick={handleMic} style={{ color: listening ? '#126ece': "#919191" }} />
+            )}
           </div>
         </div>
       </div>
